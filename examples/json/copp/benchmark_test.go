2e2ac87c86d2740b782843239ff5f39f83ab06c0
Michele Giornetta <michelegiornetta@gmail.com>
2024-09-05 17:54:08 +0200

    exp: bigger bench

--- examples/json/copp/benchmark_test.go
+++ examples/json/copp/benchmark_test.go
@@ -9,21 +9,27 @@ import (
 const baseFolder = "../data/"
 
 var entries = []*benchmark.Entry[any]{
+	//{
+	//	Filename:       baseFolder + "citylots.json",
+	//	ParallelFactor: 0.5,
+	//	AvgTokenLength: 4,
+	//	Result:         nil,
+	//},
+	//{
+	//	Filename:       baseFolder + "emojis-1000.json",
+	//	ParallelFactor: 1,
+	//	AvgTokenLength: 8,
+	//	Result:         nil,
+	//},
+	//{
+	//	Filename:       baseFolder + "wikidata-lexemes.json",
+	//	ParallelFactor: 0,
+	//	AvgTokenLength: 4,
+	//	Result:         nil,
+	//},
 	{
-		Filename:       baseFolder + "citylots.json",
+		Filename:       baseFolder + "citylots-big.json",
 		ParallelFactor: 0.5,
-		AvgTokenLength: 4,
-		Result:         nil,
-	},
-	{
-		Filename:       baseFolder + "emojis-1000.json",
-		ParallelFactor: 1,
-		AvgTokenLength: 8,
-		Result:         nil,
-	},
-	{
-		Filename:       baseFolder + "wikidata-lexemes.json",
-		ParallelFactor: 0,
 		AvgTokenLength: 4,
 		Result:         nil,
 	},
